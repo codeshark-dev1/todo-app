4363403e6b6653ea9e5424db52c6cc13897e2d17
Shark Games <codeshark2021@gmail.com>
2026-08-23 20:05:06 +0200

    fixed task button positioning

--- logic.js
+++ logic.js
@@ -6,20 +6,32 @@ class Task{
 }
 
 function onTaskButtonMouseClick(task){
-    const button = task.elements.querySelectorAll(".taskButton")[0];
     const p = task.elements.querySelectorAll(".taskText")[0];
     if (p.value == "")
         return;
 
     task.completed = !task.completed;
+    checkTaskCompletion(task);
+}
+
+function checkTaskCompletion(task){
+    const p = task.elements.querySelectorAll(".taskText")[0];
+    const button = task.elements.querySelectorAll(".taskButton")[0];
 
     if (task.completed){
+        tasksParent.insertBefore(task.elements, tasksParent.lastChild);
+
         p.style.textDecoration = "line-through";
         button.innerHTML = "X";
     }else{
         p.style.textDecoration = "none";
         button.innerHTML = "";
     }
+}
+
+function onTaskTextChanged(task){
+    task.completed = false;
+    checkTaskCompletion(task);
 }
 
 function createTaskElements(){
@@ -44,10 +56,14 @@ function createTaskElements(){
 function createNewTask(){
     const taskElements = createTaskElements();
     const task = new Task(taskElements);
+
     let button = taskElements.querySelectorAll(".taskButton")[0];
     button.onmousedown = function() {onTaskButtonMouseClick(task)};
 
-    tasks.unshift(task);
+    let p = taskElements.querySelectorAll(".taskText")[0];
+    p.onchange = function() {onTaskTextChanged(task)};
+
+    tasks.push(task);
     tasksParent.insertBefore(taskElements, tasksParent.firstChild);
 
     taskElements.style.opacity = 100;
