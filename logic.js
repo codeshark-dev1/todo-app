9528cf28a0ae67dc1a027039523fb29fc5fb83b4
Shark Games <codeshark2021@gmail.com>
2026-08-23 09:33:39 +0200

    added basic striping off tasks
    removed some bloat

--- logic.js
+++ logic.js
@@ -1,33 +1,35 @@
-function taskButtonMouseOver(x){
-    x.innerHTML = "X";
+function onTaskButtonMouseOver(x){
+    x.button.innerHTML = "X";
+    //since you click on the button, the X should already be there
+    //so no need (i think) to permanently place the X
 }
 
-function taskButtonMouseLeave(x){
-    x.innerHTML = "";
+function onTaskButtonMouseLeave(x){
+    if (!x.completed)
+        x.button.innerHTML = "";
+}
+
+function onTaskButtonMouseClick(x){
+    x.completed = !x.completed; //see note at onTaskButtonMouseOver, this function handles that logic
+    
+    let t = x.text;
+    t.style.textDecoration = x.completed ? "line-through" : "none";
 }
 
 function createNewTask(description){
-    var newTask = new Task();
-    newTask.description = description;
-    newTask.completed = false;
-
+    var newTask = new Task(description);
+    createTaskElement(newTask);
+    
     tasks.push(newTask);
-}
-
-function updateTaskButtons(){
-    const taskButtons = document.querySelectorAll(".taskButton");
-
-    for (let i = 0; i< taskButtons.length; i++){
-        const e = taskButtons[i];
-        e.onmouseover = function() {taskButtonMouseOver(e)};
-        e.onmouseleave = function() {taskButtonMouseLeave(e)};
-    }
+    
 }
 
 class Task{
     constructor(description){
         this.description = description;
         this.completed = false;
+        this.button = false;
+        this.text = null;
     }
 }
 
@@ -35,35 +37,29 @@ function createTaskElement(task){
     const newTask = taskTemplate.cloneNode(true);
 
     let p = newTask.querySelectorAll(".taskText");
-    p[0].innerHTML = task.description;
+    task.text = p[0];
+    task.text.innerHTML = task.description;
+
+    let b = newTask.querySelectorAll(".taskButton");
+
+    const e = b[0];
+    e.onmouseover = function() {onTaskButtonMouseOver(task)};
+    e.onmouseleave = function() {onTaskButtonMouseLeave(task)};
+    e.onmousedown = function() {onTaskButtonMouseClick(task)};
+
+    task.button = e;
 
     newTask.style.opacity = 100;
 
     tasksParent.appendChild(newTask);
-    updateTaskButtons();
 }
 
 const taskTemplate = document.getElementById("task");
 taskTemplate.style.opacity = 0;
 
 const tasksParent = document.getElementById("tasksParent");
-updateTaskButtons();
 
 var tasks = [];
 
 createNewTask("gym");
 createNewTask("groceries");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-createNewTask("task");
-
-
-var taskString = "";
-for (let i = 0; i < tasks.length; i++) {
-    createTaskElement(tasks[i]);
-}
